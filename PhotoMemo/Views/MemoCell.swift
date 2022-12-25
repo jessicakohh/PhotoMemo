a66b6dca4c09e025e1972cd9c5883939ce9fc78b
juyeong ko <juko95@me.com>
2022-12-25 18:39:29 +0900

    Code _ MemoCell _ CoreData

--- PhotoMemo/Views/MemoCell.swift
+++ PhotoMemo/Views/MemoCell.swift
@@ -15,10 +15,16 @@ final class MemoCell: UITableViewCell {
     @IBOutlet weak var thumbnailImage: UIImageView!
     @IBOutlet weak var updateButton: UIButton!
     
+    // 재활용 셀 중첩오류 해결법
+    override func prepareForReuse() {
+        thumbnailImage.image = nil
+        thumbnailImage.image = UIImage(named: "emptyImage")
+    }
     
     // diaryData를 전달받을 변수 (전달받으면 -> 표시하는 메서드 실행)
     var diaryData: Diary? {
         didSet {
+        configureUIwithData()
         }
     }
     
@@ -31,13 +37,20 @@ final class MemoCell: UITableViewCell {
     }
     
     func configureUI() {
-        
+        thumbnailImage.layer.cornerRadius = thumbnailImage.frame.height / 2
     }
     
     func configureUIwithData() {
-        
+        titleTextLabel.text = diaryData?.titleText
+        dateTextLabel.text = diaryData?.dateString
+        thumbnailImage.image = UIImage(data: (diaryData?.memoImage)!)
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
     }
     
     @IBAction func updateButtonPressed(_ sender: UIButton) {
+        updateButtonPressed(self)
     }
 }
