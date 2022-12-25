e1d3bf7dd06504e30ee59398516374289526d616
juyeong ko <juko95@me.com>
2022-12-25 17:49:17 +0900

    MVC

---
+++ PhotoMemo/Controllers/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  PhotoMemo
+//
+//  Created by juyeong koh on 2022/12/25.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
